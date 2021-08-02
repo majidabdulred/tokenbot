292aa32ceec2bf6d0bbac75343f02ea648147d52
Abdul Majid <abdulmajidred@gmail.com>
2021-08-02 21:48:10 +0530

    Fixed the requirements and handle error.

--- lib/handle_errors.py
+++ lib/handle_errors.py
@@ -2,15 +2,18 @@ from discord.errors import HTTPException, Forbidden
 from discord.ext.commands import CommandNotFound, BadArgument, MissingRequiredArgument, BadBoolArgument
 from pandas.core.indexing import IndexingError
 from discord_slash.error import IncorrectFormat
+
 "pymongo.errors.DuplicateKeyError: E11000 duplicate key error collection: chick.users index: _id_ dup key: { _id: 1001 }, full error: {'index': 0, 'code': 11000, 'keyPattern': {'_id': 1}, 'keyValue': {'_id': 1001}, 'errmsg': 'E11000 duplicate key error collection: chick.users index: _id_ dup key: { _id: 1001 }'}"
 
 
 async def handle_errors(exc, ctx):
-    print(f"[!] {ctx.author.name} {exc}")
     if hasattr(exc, "original"):
         Error = exc.original
     else:
         Error = exc
+    if isinstance(Error, CommandNotFound):
+        return
+    print(f"[!] {ctx.author.name} {exc}")
     if any([isinstance(Error, error) for error in (MissingRequiredArgument, BadBoolArgument, BadArgument)]):
         await ctx.send("Command not used properly.")
 
